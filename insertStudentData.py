861af171d433648de760fb9880b5428d1b133113
Gowtham88-projects <idontcarek8s@gmail.com>
2025-02-11 16:47:07 +0530

    Final Commit of student data

--- insertStudentData.py
+++ insertStudentData.py
@@ -4,12 +4,12 @@ import boto3
 # Create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # Use the DynamoDB object to select our table
-table = dynamodb.Table('Student_Info_DYDB')
+table = dynamodb.Table('studentinfo')
 
 # Define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
     # Extract values from the event object we got from the Lambda service and store in variables
-    student_id = event['studentid']
+    student_id = event['studentdetails']
     name = event['name']
     student_class = event['class']
     age = event['age']
@@ -17,7 +17,7 @@ def lambda_handler(event, context):
     # Write student data to the DynamoDB table and save the response in a variable
     response = table.put_item(
         Item={
-            'studentid': student_id,
+            'studentdetails': student_id,
             'name': name,
             'class': student_class,
             'age': age
@@ -29,3 +29,4 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Student data saved successfully!')
     }
+
--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://t58skx1h4i.execute-api.us-east-2.amazonaws.com/Testing-prod"; // This Url gets from the Api-gw Endpoint after creating it
 
 // AJAX POST request to save student data
-document.getElementById("savestudent").onclick = function(){
+document.getElementById("savestudent").onclick = function () {
     var inputData = {
         "studentid": $('#studentid').val(),
         "name": $('#name').val(),
@@ -12,7 +12,7 @@ document.getElementById("savestudent").onclick = function(){
     $.ajax({
         url: API_ENDPOINT,
         type: 'POST',
-        data:  JSON.stringify(inputData),
+        data: JSON.stringify(inputData),
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
             document.getElementById("studentSaved").innerHTML = "Student Data Saved!";
@@ -24,14 +24,14 @@ document.getElementById("savestudent").onclick = function(){
 }
 
 // AJAX GET request to retrieve all students
-document.getElementById("getstudents").onclick = function(){  
+document.getElementById("getstudents").onclick = function () {
     $.ajax({
         url: API_ENDPOINT,
         type: 'GET',
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
             $('#studentTable tr').slice(1).remove();
-            jQuery.each(response, function(i, data) {          
+            jQuery.each(response, function (i, data) {
                 $("#studentTable").append("<tr> \
                     <td>" + data['studentid'] + "</td> \
                     <td>" + data['name'] + "</td> \
